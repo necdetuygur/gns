70cb48fa58b65544daeee46d30df82ceb1dc8848
necdetuygur <necdet.uygur@gmail.com>
2020-12-17 00:46:36 +0300

    Version update

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@ cd /vagrant/data
 
 # GO
 # rm -rf /usr/bin/go
-wget https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.14.4.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.15.6.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz
 # ln -s /usr/local/go/bin/go /usr/bin/go
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 echo "export GOPATH=$HOME/go" >> ~/.bashrc
@@ -11,8 +11,8 @@ echo "export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >> ~/.bashrc
 source ~/.bashrc
 
 # NODE
-wget https://nodejs.org/dist/v12.18.0/node-v12.18.0-linux-x64.tar.xz
-tar -xf node-v12.18.0-linux-x64.tar.xz
-sudo cp -r node-v12.18.0-linux-x64/{bin,include,lib,share} /usr/
-rm -rf node-v12.18.0-linux-x64
-# rm -rf node-v12.18.0-linux-x64.tar.xz
+wget https://nodejs.org/dist/v14.15.2/node-v14.15.2-linux-x64.tar.xz
+tar -xf node-v14.15.2-linux-x64.tar.xz
+sudo cp -r node-v14.15.2-linux-x64/{bin,include,lib,share} /usr/
+rm -rf node-v14.15.2-linux-x64
+# rm -rf node-v14.15.2-linux-x64.tar.xz
